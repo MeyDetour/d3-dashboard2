a5016908327b4f177558f451eedd10c080e43a77
mey <meydetour@gmail.com>
2025-04-01 12:44:28 +0200

    feat: add dashboard

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Dashboard from "./page/dashboard.jsx";
+
+function App() {
+
+  return (
+    <>
+      <Dashboard></Dashboard>
+    </>
+  )
+}
+
+export default App
